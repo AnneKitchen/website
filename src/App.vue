337b0bcd05989eb29074a1cbf554107264b86b5c
Aries Cs <aries0d0f@gmail.com>
2017-06-06 14:58:41 +0800

    Deploy

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+const styleLoader = require('@/components/Styleloader.vue')
+export default {
+  components: {
+    styleLoader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
